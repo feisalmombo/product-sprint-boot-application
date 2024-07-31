31ff4ced6198df1fb4641ff3e76aeb0cddd2e48e
feisalmombo <feisalmombo29@gmail.com>
2024-07-31 19:47:51 +0300

    WIP

--- src/main/java/com/efetech/spring_boot_crud_project/controller/ProductController.java
+++ src/main/java/com/efetech/spring_boot_crud_project/controller/ProductController.java
@@ -1,8 +1,20 @@
 package com.efetech.spring_boot_crud_project.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.efetech.spring_boot_crud_project.entity.Product;
+import com.efetech.spring_boot_crud_project.service.ProductService;
 
 @RestController
 public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+    public Product addProduct(@RequestBody Product product) {
+        return service.saveProduct(product);
+    }
 
 }
