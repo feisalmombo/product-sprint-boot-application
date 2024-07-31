93fcbd3e8fc2b32bc26d8070b1a729dce6b1efcc
feisalmombo <feisalmombo29@gmail.com>
2024-07-31 15:55:46 +0300

    WIP

--- src/main/java/com/efetech/spring_boot_crud_project/service/ProductService.java
+++ src/main/java/com/efetech/spring_boot_crud_project/service/ProductService.java
@@ -1,5 +1,18 @@
 package com.efetech.spring_boot_crud_project.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.efetech.spring_boot_crud_project.entity.Product;
+import com.efetech.spring_boot_crud_project.repository.ProductRepository;
+
+@Service
 public class ProductService {
 
+    @Autowired // This part used to inject from repository
+    private ProductRepository repository;
+
+    public Product saveProduct(Product product) {
+        return repository.save(product);
+    }
 }
