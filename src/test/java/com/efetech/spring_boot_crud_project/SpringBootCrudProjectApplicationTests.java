f2d74eb58fcc8493345f529faed027819e3b72c7
feisalmombo <feisalmombo29@gmail.com>
2024-07-31 12:27:22 +0300

    First Commit

---
+++ src/test/java/com/efetech/spring_boot_crud_project/SpringBootCrudProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.efetech.spring_boot_crud_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
