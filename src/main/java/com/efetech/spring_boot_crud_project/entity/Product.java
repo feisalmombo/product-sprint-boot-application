49fc865ec653346fb3a14ca1f803c3885d00a3a6
feisalmombo <feisalmombo29@gmail.com>
2024-07-31 13:49:56 +0300

    create product repository

--- src/main/java/com/efetech/spring_boot_crud_project/entity/Product.java
+++ src/main/java/com/efetech/spring_boot_crud_project/entity/Product.java
@@ -1,5 +1,25 @@
 package com.efetech.spring_boot_crud_project.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "PRODUCT_TBL")
 public class Product {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+    private int quantity;
+    private double price;
 
 }
