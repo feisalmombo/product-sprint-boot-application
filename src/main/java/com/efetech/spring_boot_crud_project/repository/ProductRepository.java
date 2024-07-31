3384b2c1797224065d03f6289a4b184169de34d1
feisalmombo <feisalmombo29@gmail.com>
2024-07-31 15:34:22 +0300

    WIP

--- src/main/java/com/efetech/spring_boot_crud_project/repository/ProductRepository.java
+++ src/main/java/com/efetech/spring_boot_crud_project/repository/ProductRepository.java
@@ -1,5 +1,9 @@
 package com.efetech.spring_boot_crud_project.repository;
 
-public class ProductRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.efetech.spring_boot_crud_project.entity.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
 
 }
