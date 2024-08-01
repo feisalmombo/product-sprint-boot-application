fc735f2182b4ab596ce3dbfaa62518c62b8b0c51
feisalmombo <feisalmombo29@gmail.com>
2024-08-01 14:26:34 +0300

    WIP

--- src/main/java/com/efetech/spring_boot_crud_project/controller/ProductController.java
+++ src/main/java/com/efetech/spring_boot_crud_project/controller/ProductController.java
@@ -3,9 +3,11 @@ package com.efetech.spring_boot_crud_project.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -41,6 +43,16 @@ public class ProductController {
     @GetMapping("/product/{name}")
     public Product findProductByName(@PathVariable String name) {
         return service.getProductByName(name);
+    }
+
+    @PutMapping("/update")
+    public Product updateProduct(@RequestBody Product product) {
+        return service.updateProduct(product);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteProduct(@PathVariable int id) {
+        return service.deleteProduct(id);
     }
 
 }
