c7754d4ebe0749e223e677dd92e230e3af5ceac5
feisalmombo <feisalmombo29@gmail.com>
2024-08-01 12:27:43 +0300

    WIP

--- src/main/java/com/efetech/spring_boot_crud_project/controller/ProductController.java
+++ src/main/java/com/efetech/spring_boot_crud_project/controller/ProductController.java
@@ -3,6 +3,8 @@ package com.efetech.spring_boot_crud_project.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +26,21 @@ public class ProductController {
     @PostMapping("/addProducts")
     public List<Product> addProducts(@RequestBody List<Product> products) {
         return service.saveProducts(products);
+    }
+
+    @GetMapping("products")
+    public List<Product> findAllProducts() {
+        return service.getProducts();
+    }
+
+    @GetMapping("/product/{id}")
+    public Product findProductById(@PathVariable int id) {
+        return service.getProductById(id);
+    }
+
+    @GetMapping("/product/{name}")
+    public Product findProductByName(@PathVariable String name) {
+        return service.getProductByName(name);
     }
 
 }
