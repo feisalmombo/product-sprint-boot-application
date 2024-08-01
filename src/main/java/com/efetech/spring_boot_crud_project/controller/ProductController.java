41e020a0356b4c5c925fdad861f69bd39fb78b4a
feisalmombo <feisalmombo29@gmail.com>
2024-08-01 11:59:29 +0300

    WIP

--- src/main/java/com/efetech/spring_boot_crud_project/controller/ProductController.java
+++ src/main/java/com/efetech/spring_boot_crud_project/controller/ProductController.java
@@ -1,6 +1,9 @@
 package com.efetech.spring_boot_crud_project.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +16,14 @@ public class ProductController {
     @Autowired
     private ProductService service;
 
+    @PostMapping("/addProduct")
     public Product addProduct(@RequestBody Product product) {
         return service.saveProduct(product);
+    }
+
+    @PostMapping("/addProducts")
+    public List<Product> addProducts(@RequestBody List<Product> products) {
+        return service.saveProducts(products);
     }
 
 }
